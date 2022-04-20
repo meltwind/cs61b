3be1e53ffcc2f6f7de5de9ed19b4f87c604cc23b
luo <1931976629@qq.com>
2022-04-20 09:18:41 +0800

    update

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -98,6 +98,6 @@ public class Planet {
     }
     public void draw()
     {
-        StdDraw.picture(xxPos,yyPos,imgFileName);
+        StdDraw.picture(xxPos,yyPos,"images/"+imgFileName);
     }
 }
