d72fa075e7d964cd38903b35d1e1bb2ebdf8224c
luo <1931976629@qq.com>
2022-04-23 20:04:28 +0800

    proj1a

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,128 @@
+public class ArrayDeque<T> {
+    /** Creates an empty list. */
+    private T []items;
+    private static  int capaticy=8;
+    private int right;
+    private int left;
+
+    public ArrayDeque() {
+        items=(T []) new Object[capaticy];
+        left=right=0;
+    }
+
+    /** Inserts X into the back of the list. */
+    private void resize(int cap)
+    {
+        T []newarray=(T [])new Object[cap];
+        int size=size();
+        if(right>left)
+        {
+            System.arraycopy(items,left,newarray,0,size);
+
+        }else {
+            System.arraycopy(items,left,newarray,0,size-left);
+            System.arraycopy(items,0,newarray,size-left,right);
+        }
+        left=0;
+        right=size;
+        items=newarray;
+        capaticy=cap;
+    }
+    private boolean isFull() {
+        return size() == capaticy - 1;
+    }
+    public void  addFirst(T x)
+    {
+        if(isFull())
+        {
+            resize((int)(capaticy*2));
+        }
+        left=(left-1+capaticy)%capaticy;
+        items[left]=x;
+    }
+
+    public void addLast(T x) {
+        if(isFull())
+        {
+            resize((int)(capaticy*2));
+        }items[right]=x;
+        right=(right+1+capaticy)%capaticy;
+
+    }
+
+    /** Returns the item from the back of the list. */
+    public T getLast() {
+        return items[right-1];
+    }
+    public T getFirst()
+    {
+        return items[left];
+    }
+    /** Gets the ith item in the list (0 is the front). */
+    public T get(int i) {
+        return items[(left+i)%capaticy];
+    }
+    public boolean isEmpty()
+    {
+        return (left==right);
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return (right-left+capaticy)%capaticy;
+    }
+    public T removeLast()
+    {
+        if(isEmpty())
+        {
+            return null;
+        }
+        T result=items[(right-1+capaticy)%capaticy];
+        items[(right-1+capaticy)%capaticy]=null;
+        right=(right-1+capaticy)%capaticy;
+        if (isLowUsageRate()) {
+            resize((int) (capaticy * 0.5));
+        }
+        return result;
+
+    }
+    public T removeFirst()
+    {
+        if(isEmpty())
+        {
+            return null;
+        }
+        T result=items[left];
+        items[left]=null;
+        left=(left+1+capaticy)%capaticy;
+        if (isLowUsageRate()) {
+            resize((int) (capaticy * 0.5));
+        }
+        return  result;
+
+    }
+    private boolean  isLowUsageRate()
+    {
+        return capaticy>16&&(this.size()/(double)capaticy<0.25);
+    }
+    public void printDeque()
+    {
+        for(int i=0;i<size();i++)
+        {
+            System.out.print(get(i));
+            System.out.print(" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ls=new ArrayDeque();
+        ls.addFirst(1);
+        ls.addLast(2);
+        ls.addFirst(3);
+       ls.addFirst(5);
+       ls.addLast(7);
+      ls.removeLast();
+        ls.printDeque();
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,132 @@
+import sun.awt.image.ImageWatched;
+
+public class LinkedListDeque<T> {
+
+    private IntNode sentinel = new IntNode(null, null, null);
+    private int size;
+
+    private  class IntNode {
+        public IntNode prev;
+        public T item;
+        public IntNode next;
+
+        public IntNode(IntNode p, T it, IntNode n) {
+            prev = p;
+            item = it;
+            next = n;
+
+        }
+
+    }
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;//圈
+
+
+    }
+
+    public LinkedListDeque(T x) {
+        size = 1;
+        IntNode newnode = new IntNode(sentinel, x, sentinel);
+        sentinel.next = newnode;
+        sentinel.prev = newnode;
+
+    }
+
+    public void addFirst(T item) {
+        size += 1;
+        IntNode newnode = new IntNode(sentinel, item, sentinel.next);
+        sentinel.next.prev = newnode;
+        sentinel.next = newnode;
+
+
+    }
+
+    public void addLast(T item) {
+        size += 1;
+        IntNode newnode = new IntNode(sentinel.prev, item, sentinel);
+        sentinel.prev.next = newnode;
+        sentinel.prev = newnode;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        T result = sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        size -= 1;
+        return result;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        T result = sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size -= 1;
+        return result;
+    }
+
+    public T get(int index) {
+        if (index > size - 1) {
+            return null;
+        }
+        int flag = 0;
+        IntNode p = sentinel.next;
+        while (index != flag) {
+
+            flag += 1;
+            p = p.next;
+        }
+        return p.item;
+
+    }
+    private  T gethelp(IntNode p,int index)
+    {
+        if(index==0)
+        {
+            return  p.item;
+        }else
+        {
+            return gethelp(p.next,index-1);
+        }
+    }
+    public T getRecursive(int index)
+    {
+        return gethelp(sentinel.next,index);
+    }
+
+    public void printDeque() {
+        IntNode p = sentinel.next;
+        for (int i = 0; i < size; i++) {
+            System.out.print(p.item);
+            System.out.print(" ");
+            p = p.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListDeque<Integer> ls = new LinkedListDeque<Integer>();
+        ls.addLast(3);
+        ls.addFirst(4);
+        ls.addFirst(5);
+        ls.addFirst(6);
+        System.out.println(ls.getRecursive(3));
+
+
+    }
+}
