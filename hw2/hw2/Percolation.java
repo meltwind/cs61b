3cdddcdcc7dee4ff5af07450bf060db749a0a349
luo <1931976629@qq.com>
2022-05-31 21:49:39 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,134 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] Array;
+    private int openSize;
+    private int nn;
+    private WeightedQuickUnionUF un;
+    private WeightedQuickUnionUF un2;
 
+    Percolation(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        un = new WeightedQuickUnionUF(n * n + 2);
+        un2 = new WeightedQuickUnionUF(n * n + 1);
+        Array = new boolean[n][n];
+        openSize = 0;
+        nn = n * n;
+    }
+
+    public void open(int row, int col) {
+        validateRange(row, col);
+        if (isOpen(row, col)) {
+            return;
+        }
+        Array[row][col] = true;
+        connect(row, col);
+        openSize += 1;
+    }
+
+    public boolean isOpen(int row, int col) {
+        validateRange(row, col);
+        return Array[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        validateRange(row, col);
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        int num = num(row, col);
+        return un2.connected(nn, num(row, col));
+    }
+
+    public int numberOfOpenSites() {
+        return openSize;
+    }
+
+    /*return the index of Array site*/
+    private int num(int row, int col) {
+        int lon = Array[0].length;
+        return lon * row + col;
+    }
+
+    public boolean percolates() {
+        return un.connected(nn, nn + 1);
+    }
+
+    private boolean inTop(int num) {
+        return (num >= 0 && num < Array[0].length);
+
+    }
+
+    private boolean inBottom(int num) {
+        return (num >= (Array.length - 1) * Array[0].length && num < Array.length * Array[0].length);
+    }
+
+    /*check up,left,right has full*/
+    private boolean bound(int num) {
+        if (un2.connected(nn, num + 1) || un2.connected(nn, num - 1)
+                || un2.connected(nn, num - Array[0].length) || (!inBottom(num) && un2.connected(nn, num + Array[0].length))) {
+            return true;
+        }
+        return false;
+    }
+
+    /*connect full site around,if in the top wall,connect topSentinel nn,bottom connect bottomSentinel nn+1*/
+    private void connect(int row, int col) {
+        int num = num(row, col);
+        if (inTop(num)) {
+            un.union(nn, num);
+            un2.union(nn, num);
+        } else if (inBottom(num)) {
+            un.union(nn + 1, num);
+            if (bound(num)) {
+                un.union(nn, num);
+                un2.union(nn, num);
+            }
+
+        } else {
+            if (bound(num)) {
+                un.union(nn, num);
+                un2.union(nn, num);
+            }
+        }
+        connectPrevious(row, col);
+    }
+
+    private void validateRange(int row, int col) {
+        if (row < 0 || row >= Array[0].length || col < 0 || col >= Array.length) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void connectPrevious(int row, int col) {
+        if (isFull(row, col)) {
+            int num = num(row, col);
+            if (col != 0 && (!un2.connected(nn, num - 1)) && isOpen(row, col - 1)) {
+                un.union(nn, num - 1);
+                un2.union(nn, num - 1);
+                connectPrevious(row, col - 1);
+            }
+            if (col != Array[0].length - 1 && (!un2.connected(nn, num + 1)) && isOpen(row, col + 1)) {
+                un.union(nn, num + 1);
+                un2.union(nn, num + 1);
+                connectPrevious(row, col + 1);
+            }
+            if (row != 0 && (!un2.connected(nn, num - Array[0].length)) && isOpen(row - 1, col)) {
+                un.union(nn, num - Array[0].length);
+                un2.union(nn, num - Array[0].length);
+                connectPrevious(row - 1, col);
+            }
+            if (row != Array.length - 1 && (!un2.connected(nn, num + Array[0].length)) && isOpen(row + 1, col)) {
+                un.union(nn, num + Array[0].length);
+                un2.union(nn, num + Array[0].length);
+                connectPrevious(row + 1, col);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,46 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private double[] array;
+    private int T;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N < 0 || T < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.T = T;
+        array = new double[T];
+        Percolation pe = pf.make(N);
+        for (int i = 0; i < T; i++) {
+            while (!pe.percolates()) {
+                int x, y;
+                do {
+                    x = StdRandom.uniform(N);
+                    y = StdRandom.uniform(N);
+                } while (pe.isOpen(x, y));
+                pe.open(x, y);
+            }
+            array[i] = (double) pe.numberOfOpenSites() / (N * N);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(array);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(array);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 
 }
