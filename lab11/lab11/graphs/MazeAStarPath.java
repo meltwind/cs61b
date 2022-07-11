c0523f457049170b43750a54040a25174e351368
luo <1931976629@qq.com>
2022-07-11 15:15:22 +0800

    lab11

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,7 +1,12 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
@@ -18,25 +23,59 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
-    /** Estimate of the distance from v to the target. */
+    /**
+     * Estimate of the distance from v to the target.
+     */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
-    /** Finds vertex estimated to be closest to target. */
+    /**
+     * Finds vertex estimated to be closest to target.
+     */
     private int findMinimumUnmarked() {
         return -1;
         /* You do not have to use this method. */
     }
 
-    /** Performs an A star search from vertex s. */
+    /**
+     * Performs an A star search from vertex s.
+     */
     private void astar(int s) {
-        // TODO
+        marked[s] = true;
+        announce();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return (distTo[o1] + h(o1)) - (distTo[o2] + h(o2));
+            }
+        });
+        pq.add(s);
+        while (!pq.isEmpty()) {
+            int source = pq.poll();
+            for (int w : maze.adj(source)) {
+                if (!marked[w]) {
+                    edgeTo[w] = source;
+                    distTo[w] = distTo[source] + 1;
+                    marked[w] = true;
+                    announce();
+                    if (w == t) {
+                        targetFound = true;
+                    }
+                    if (targetFound) {
+                        return;
+                    }
+                    pq.add(w);
+                }
+            }
+        }
+
     }
 
     @Override
     public void solve() {
         astar(s);
+
     }
 
 }
--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +14,52 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int v) {
+        marked[v] = true;
+        announce();
+        Queue<Integer> fringe = new ArrayDeque<>();
+        fringe.add(v);
+        while (!fringe.isEmpty()){
+            int source = fringe.poll();
+            for(int w: maze.adj(source)){
+                if(!marked[w]){
+                    distTo[w] = distTo[source] + 1;
+                    edgeTo[w] = source;
+                    marked[w] = true;
+                    fringe.add(w);
+                    announce();
+                    if (w == t) {
+                        targetFound = true;
+                    }
+
+                    if (targetFound) {
+                        return;
+                    }
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs(s);
     }
 }
 
